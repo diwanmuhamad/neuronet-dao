448327b25fe089cc84f79a63800cc21fdc134a5c
ddewantaraq <muhammad.dimas.dewantara@gmail.com>
2025-09-07 14:10:01 +0700

    remove unnecessary pages

--- neuronet-dao-frontend/public/data/navbar-data.ts
+++ neuronet-dao-frontend/public/data/navbar-data.ts
@@ -1,36 +1,7 @@
 const NavbarData = [
   {
     title: "Home",
-    submenu: [
-      {
-        title: "Ai Solution",
-        path: "/",
-      },
-      {
-        title: "Ai Chatbot",
-        path: "/index-two",
-      },
-      {
-        title: "Ai Gaming",
-        path: "/index-three",
-      },
-      {
-        title: "Ai Prompts",
-        path: "/index-four",
-      },
-      {
-        title: "Ai Portfolio",
-        path: "/index-five",
-      },
-      {
-        title: "Ai News",
-        path: "/index-six",
-      },
-      {
-        title: "Ai Agency",
-        path: "/index-seven",
-      },
-    ],
+    path: "/",
   },
   {
     title: "About Us",
@@ -67,49 +38,6 @@ const NavbarData = [
       {
         title: "Sign In",
         path: "/sign-in",
-      },
-      {
-        title: "Services",
-        subInSub: [
-          {
-            title: "Our Services",
-            path: "/services",
-          },
-          {
-            title: "Service Details",
-            path: "/service-single",
-          },
-        ],
-      },
-      {
-        title: "Case Study",
-        subInSub: [
-          {
-            title: "Case Study",
-            path: "/case-study",
-          },
-          {
-            title: "Case Study Details",
-            path: "/case-study-single",
-          },
-        ],
-      },
-      {
-        title: "Create Account",
-        path: "/sign-up",
-      },
-    ],
-  },
-  {
-    title: "Blog",
-    submenu: [
-      {
-        title: "Blog",
-        path: "/blog",
-      },
-      {
-        title: "Blog Details",
-        path: "/blog-single",
       },
     ],
   },
