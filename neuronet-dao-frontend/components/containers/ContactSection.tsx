141338ccf598a69712bff9b81b131d23dd31196d
ilhamdirga22 <idirga37@gmail.com>
2025-09-16 03:35:45 +0800

    update contact us part 2

--- neuronet-dao-frontend/components/containers/ContactSection.tsx
+++ neuronet-dao-frontend/components/containers/ContactSection.tsx
@@ -10,21 +10,17 @@ const ContactSection = () => {
     <section className="section m-contact fade-wrapper">
       <div className="container">
         <div className="row gaper section pt-0">
+          {/* Email */}
           <div className="col-12 col-sm-6 col-lg-4">
             <div className="m-contact__single fade-top">
               <div className="thumb">
-                <Image src={mail} alt="Image" priority />
+                <Image src={mail} alt="Email Icon" priority />
               </div>
               <div className="content">
                 <h3>Email</h3>
                 <p>
-                  <Link href="mailto:alma.lawson@example.com">
-                    alma.lawson@example.com
-                  </Link>
-                </p>
-                <p>
-                  <Link href="mailto:michael.mitc@example.com">
-                    michael.mitc@example.com
+                  <Link href="mailto:idirga37@gmail.com">
+                    idirga37@gmail.com
                   </Link>
                 </p>
               </div>
@@ -33,15 +29,12 @@ const ContactSection = () => {
           <div className="col-12 col-sm-6 col-lg-4">
             <div className="m-contact__single fade-top">
               <div className="thumb">
-                <Image src={phone} alt="Image" priority />
+                <Image src={phone} alt="Phone Icon" priority />
               </div>
               <div className="content">
                 <h3>Phone</h3>
                 <p>
-                  <Link href="tel:219-555-0114">(480) 555-0103</Link>
-                </p>
-                <p>
-                  <Link href="tel:219-555-0114">(219) 555-0114</Link>
+                  <Link href="tel:+6281339137304">+62 813-3913-7304</Link>
                 </p>
               </div>
             </div>
@@ -49,24 +42,48 @@ const ContactSection = () => {
           <div className="col-12 col-sm-6 col-lg-4">
             <div className="m-contact__single fade-top">
               <div className="thumb">
-                <Image src={location} alt="Image" priority />
+                <Image src={location} alt="Location Icon" priority />
               </div>
               <div className="content">
                 <h3>Location</h3>
                 <p>
-                  <Link href="tel:219-555-0114" target="_blank">
-                    4517 Washington Ave. Manchester, Kentucky 39495
+                  <Link
+                    href="https://maps.google.com/?q=Denpasar,Bali,Indonesia"
+                    target="_blank"
+                  >
+                    Denpasar, Bali, Indonesia
                   </Link>
                 </p>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row gaper align-items-center">
+          <div className="col-12 col-sm-6 col-lg-4">
+            <div className="m-contact__single fade-top">
+              <div className="thumb">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M18.244 2H21.5l-7.59 8.69L22 22h-6.845l-5.364-6.67L4.5 22H1.243l8.088-9.26L2 2h6.927l4.846 5.91L18.244 2z" />
+                </svg>
+              </div>
+              <div className="content">
+                <h3>X (Twitter)</h3>
+                <p>
+                  <Link href="https://x.com/NeuronetD62150" target="_blank">
+                    @NeuronetD62150
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
           <div className="col-12 col-lg-6 col-xl-6">
             <div className="m-contact__form">
               <h3 className="title-animation fw-7 text-white text-uppercase mt-12">
-                PLEASE MESSAGE ME, IF YOU HAVE ANY QUERIES
+                PLEASE MESSAGE US, IF YOU HAVE ANY QUERIES
               </h3>
               <form action="#" method="post">
                 <div className="input-single">
@@ -115,7 +132,7 @@ const ContactSection = () => {
           </div>
           <div className="col-12 col-lg-6 col-xl-5 offset-xl-1">
             <div className="m-contact__thumb reveal-img parallax-img">
-              <Image src={thumb} alt="Image" priority />
+              <Image src={thumb} alt="Contact Illustration" priority />
             </div>
           </div>
         </div>
